711722f5f71c57b4a40be42ed6995b0a0c4c0a9f
Ivan Orellana Jr <iorellana21@gmail.com>
2021-04-26 19:39:12 -0400

    fixing service worker - offline

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/index.js',
+    '/db.js',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
@@ -62,4 +63,12 @@ self.addEventListener("fetch", function (evt) {
         );
         return;
     }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
 });
